d5744aeaade3f61648c06d0ff5a87df06d87ed99
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-12 04:47:49 +0300

    extending JpaRepository by specifying the table and the primary key

--- src/main/java/com/willis/Quiz/App/Demo/dao/QuestionDao.java
+++ src/main/java/com/willis/Quiz/App/Demo/dao/QuestionDao.java
@@ -1,4 +1,7 @@
 package com.willis.Quiz.App.Demo.dao;
 
-public interface QuestionDao {
+import com.willis.Quiz.App.Demo.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionDao extends JpaRepository<Question,Integer> {
 }
