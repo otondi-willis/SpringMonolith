2642bd57c2ce54a6d425b24368b49bae5ecb63ea
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-12 04:30:40 +0300

    Question class created and attributes added

---
+++ src/main/java/com/willis/Quiz/App/Demo/Question.java
@@ -0,0 +1,14 @@
+package com.willis.Quiz.App.Demo;
+
+public class Question {
+
+    private Integer id;
+    private String difficultyLevel;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    private String questionTitle;
+    private String rightAnswer;
+
+}
