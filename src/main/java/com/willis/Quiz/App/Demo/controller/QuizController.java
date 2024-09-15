82e68d46520fd5aa897b4676abf7ee91325436bc
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-15 04:59:36 +0300

    feat: Adding variables accepted by the url through @RequestParam

--- src/main/java/com/willis/Quiz/App/Demo/controller/QuizController.java
+++ src/main/java/com/willis/Quiz/App/Demo/controller/QuizController.java
@@ -4,13 +4,14 @@ import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("quiz")
 public class QuizController {
     @PostMapping("create")
-    public ResponseEntity<String> createQuiz(){
+    public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
 
     }
 }
