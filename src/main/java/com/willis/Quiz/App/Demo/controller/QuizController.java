1cb81fdf565f6e324117a245bb62f8c4892dc36e
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-15 04:56:12 +0300

    feat: Added controller to create quiz

---
+++ src/main/java/com/willis/Quiz/App/Demo/controller/QuizController.java
@@ -0,0 +1,16 @@
+package com.willis.Quiz.App.Demo.controller;
+
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("quiz")
+public class QuizController {
+    @PostMapping("create")
+    public ResponseEntity<String> createQuiz(){
+
+    }
+}
