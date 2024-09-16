015266e4ed74d1026cb5a699a6d24e3f665515a0
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-16 03:41:43 +0300

    service,dao: creation of findRandomQuestionsByCategory functionality

--- src/main/java/com/willis/Quiz/App/Demo/dao/QuestionDao.java
+++ src/main/java/com/willis/Quiz/App/Demo/dao/QuestionDao.java
@@ -2,9 +2,14 @@ package com.willis.Quiz.App.Demo.dao;
 
 import com.willis.Quiz.App.Demo.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface QuestionDao extends JpaRepository<Question,Integer> {
     List<Question> findByCategory(String category);
+
+    @Query(value="SELECT * FROM question q Where " +
+            "q.category=:category ORDER BY RANDOM() LIMIT:numQ", nativeQuery = true)
+    List<Question> findRandomQuestionsByCategory(String category, int numQ);
 }
--- src/main/java/com/willis/Quiz/App/Demo/service/QuizService.java
+++ src/main/java/com/willis/Quiz/App/Demo/service/QuizService.java
@@ -5,6 +5,7 @@ import com.willis.Quiz.App.Demo.dao.QuizDao;
 import com.willis.Quiz.App.Demo.model.Question;
 import com.willis.Quiz.App.Demo.model.Quiz;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +22,10 @@ public class QuizService {
         List<Question> questions = questionDao.findRandomQuestionsByCategory(category,numQ);
         Quiz quiz = new Quiz();
         quiz.setTitle(title);
-        quiz.setQuestions();
+        quiz.setQuestions(questions);
+        quizDao.save(quiz);
+
+        return new ResponseEntity<>("success", HttpStatus.CREATED);
+
     }
 }
