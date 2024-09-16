934588bc7f793106da4f58d0777ef6173876d113
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-16 03:32:15 +0300

    controller: creation of createQuiz functionality

--- src/main/java/com/willis/Quiz/App/Demo/controller/QuizController.java
+++ src/main/java/com/willis/Quiz/App/Demo/controller/QuizController.java
@@ -17,6 +17,6 @@ public class QuizController {
     public ResponseEntity<String> createQuiz(@RequestParam String category,
                                              @RequestParam int numQ,
                                              @RequestParam String title){
-        return quizService.;
+        return quizService.createQuiz(category,numQ,title);
     }
 }
