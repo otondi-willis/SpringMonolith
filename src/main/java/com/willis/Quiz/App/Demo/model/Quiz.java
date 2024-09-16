7dd67cbd32ae89c0ccf9a0f9f48a07379d50eae6
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-16 03:25:54 +0300

    feat: use of @ManyToMany annotation

--- src/main/java/com/willis/Quiz/App/Demo/model/Quiz.java
+++ src/main/java/com/willis/Quiz/App/Demo/model/Quiz.java
@@ -1,9 +1,6 @@
 package com.willis.Quiz.App.Demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.List;
@@ -15,5 +12,6 @@ public class Quiz {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String title;
+    @ManyToMany
     private List<Question> questions;
 }
