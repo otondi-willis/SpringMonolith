bacad26435bc561750c2091e4e07d6057e37c39e
otondi-willis <willis.otondi.ke@gmail.com>
2024-11-11 04:33:11 +0300

    feat: Added comments to the main file

--- src/main/java/com/willis/Quiz/App/Demo/QuizAppDemoApplication.java
+++ src/main/java/com/willis/Quiz/App/Demo/QuizAppDemoApplication.java
@@ -3,6 +3,9 @@ package com.willis.Quiz.App.Demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Quiz App
+ * Monolith Application*/
 @SpringBootApplication
 public class QuizAppDemoApplication {
 
